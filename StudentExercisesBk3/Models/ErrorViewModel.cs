a5a98bd3e326ec94a803deed6c4e53cdb345df97
Your Name <you@example.com>
2019-08-12 18:25:55 -0500

    added code

---
+++ StudentExercisesBk3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StudentExercisesBk3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ StudentExercisesBk3/Models/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExercisesBk3.Models
+{
+    public class Student
+    {
+        [Required]
+        [Display(Name = "Student Id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Slack Handle")]
+        public string SlackHandle { get; set; }
+
+        [Required]
+        [Display(Name = "Cohort Id")]
+        public int CohortId { get; set; }
+
+        //'computed property'
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+    }
+}
