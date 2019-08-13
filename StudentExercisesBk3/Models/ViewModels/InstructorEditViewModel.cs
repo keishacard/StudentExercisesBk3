44daabca325621bed9888ab9b642fda084b9b28e
Your Name <you@example.com>
2019-08-13 13:45:29 -0500

    instructoreditviewmodel and instructorcontroller

---
+++ StudentExercisesBk3/Models/ViewModels/InstructorEditViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Configuration;
+using StudentExercisesBk3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace StudentExerciseMVC.Models.ViewModels
+{
+    public class InstructorEditViewModel
+    {
+        private readonly IConfiguration _config;
+
+        public List<SelectListItem> Cohorts { get; set; }
+        public Instructor Instructor { get; set; }
+
+        public InstructorEditViewModel() { }
+
+        public InstructorEditViewModel(IConfiguration config)
+        {
+
+            /*
+                Query the database to get all cohorts
+            */
+
+
+            /*
+                Use the LINQ .Select() method to convert
+                the list of Cohort into a list of SelectListItem
+                objects
+            */
+        }
+    }
+}
