c2d77106d7cf5e361f50e41c138f61b1f7589d64
Your Name <you@example.com>
2019-08-13 09:49:24 -0500

    added instructor controller, view models

--- StudentExercisesBk3/Controllers/HomeController.cs
+++ StudentExercisesBk3/Controllers/HomeController.cs
@@ -4,15 +4,25 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using StudentExercises.Models.ViewModels;
 using StudentExercisesBk3.Models;
 
 namespace StudentExercisesBk3.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration _config;
+
+        public HomeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var ViewModel = new StudentInstructorViewModel(_config.GetConnectionString("DefaultConnection"));
+            return View(ViewModel);
         }
 
         public IActionResult Privacy()
---
+++ StudentExercisesBk3/Models/ViewModels/StudentInstructorViewModel.cs
@@ -0,0 +1,102 @@
+﻿using StudentExercisesBk3.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExercises.Models.ViewModels
+{
+    public class StudentInstructorViewModel
+    {
+
+        public List<Student> Students { get; set; }
+        public List<Instructor> Instructors { get; set; }
+
+        private string _connectionString;
+
+        private SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_connectionString);
+            }
+        }
+
+        //'constructor making sql query to DB and what it gets back will populate list'
+        //you would prob usually call your controler to do this tho
+        public StudentInstructorViewModel(string connectionString)
+        {
+            _connectionString = connectionString;
+            GetAllStudents();
+            GetAllInstructors();
+        }
+
+        private void GetAllStudents()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT
+                            Id,
+                            FirstName,
+                            LastName,
+                            SlackHandle
+                        FROM Student";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Students = new List<Student>();
+                    while (reader.Read())
+                    {
+                        Students.Add(new Student
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            SlackHandle = reader.GetString(reader.GetOrdinal("SlackHandle")),
+                        });
+                    }
+
+                    reader.Close();
+                }
+            }
+        }
+
+        private void GetAllInstructors()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            SELECT
+                            Id,
+                            FirstName,
+                            LastName,
+                            SlackHandle
+                        FROM Instructor";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Instructors = new List<Instructor>();
+                    while (reader.Read())
+                    {
+                        Instructors.Add(new Instructor
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            SlackHandle = reader.GetString(reader.GetOrdinal("SlackHandle")),
+                        });
+                    }
+
+                    reader.Close();
+                }
+            }
+        }
+    }
+}
